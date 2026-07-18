da88556c06ba018d1c84927f3cf9d01b1246a6cf
mirazfaruk <mirazfaruk@gmail.com>
2026-07-19 03:21:54 +0600

    feat: implement delete and update functionality

--- index.js
+++ index.js
@@ -125,11 +125,43 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/menu/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: id };
+            const result = await menuCollection.findOne(query);
+            res.send(result);
+        });
+
         app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
             const newItem = req.body;
+            newItem._id = new ObjectId().toString();
             const result = await menuCollection.insertOne(newItem);
             res.send(result);
-        })
+        });
+
+        app.patch('/menu/:id', async (req, res) => {
+            const item = req.body;
+            const id = req.params.id;
+            const filter = { _id: id };
+            const updatedDoc = {
+                $set: {
+                    name: item.name,
+                    category: item.category,
+                    price: item.price,
+                    recipe: item.recipe,
+                    image: item.image
+                }
+            }
+            const result = await menuCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        });
+
+        app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: id };
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
+        });
 
         app.get('/reviews', async (req, res) => {
             const result = await reviewsCollection.find().toArray();
